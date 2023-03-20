a8d96bd8d317e8a3bb189a37b7b540e5d5735622
Patrick Brain <70504307+Jeg-Marka@users.noreply.github.com>
2023-03-20 15:24:19 +1100

    Updated
    to setint

--- app/src/main/java/workshop05code/SQLiteConnectionManager.java
+++ app/src/main/java/workshop05code/SQLiteConnectionManager.java
@@ -134,7 +134,7 @@ public class SQLiteConnectionManager {
         
         try (Connection conn = DriverManager.getConnection(databaseURL);
                 PreparedStatement pstmt = conn.prepareStatement(sql)) {
-                    pstmt.setString(1, id);
+                    pstmt.setInt(1, id);
                     pstmt.setString(2, word);
             pstmt.executeUpdate();
         } catch (SQLException e) {
